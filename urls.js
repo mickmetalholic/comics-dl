66f422dba88886b78d8d93356f395c1141715b29
yuanshenmick <yuanshenmick@gmail.com>
2019-05-01 18:20:36 +0800

    remove useless codes

--- urls.js
+++ urls.js
@@ -1,13 +1,9 @@
 // https://manhua.dmzj.com/
 const dmzjUrls = [
-  'https://manhua.dmzj.com/woshigangtiexia',
-  'https://manhua.dmzj.com/zjmhxlgtx'
 ]
 
 // https://www.manhuagui.com/
 const manhuaguiUrls = [
-  'https://www.manhuagui.com/comic/7553/',
-  'https://www.manhuagui.com/comic/17680/'
 ]
 
 // https://www.dongmanmanhua.cn/
